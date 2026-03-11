f01a579457b56c07c267c67e6319e48869bead46
historiasmaridiaz <166438539+historiasmaridiaz@users.noreply.github.com>
2026-03-11 15:07:35 -0500

    Update spreadsheet ID in graphics.js

--- js/graphics.js
+++ js/graphics.js
@@ -494,7 +494,7 @@ function openGoogleSheetsForPDF() {
         button.innerHTML = '⏳ Abriendo Google Sheets...';
         
         // Configuración
-        const spreadsheetId = '1qraItHZYo4jxLjwf1su9hOxu9OSLhi0mUSY8A3EcB94';
+        const spreadsheetId = '1YRAztDSETnV5GcsPhtfrvKM-8k922XxzUcLHiLHwBcI';
         const gid = '9685068'; // GRAFICO_FORMATO
             
         // URL de Google Sheets
